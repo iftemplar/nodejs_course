331bf869a11cf56cbbb32fb609461345968ac416
iftemplar <iftemplar@gmail.com>
2017-04-12 21:52:55 +0300

    Requiring and using functions from own files

--- app.js
+++ app.js
@@ -2,17 +2,14 @@ console.log('Starting app');
 
 const fs = require('fs'); // fs (File System) is a built in Node.js module
 const os = require('os'); // also built in module
+const notes = require('./notes.js');
 
-//getting OS user info
-var user = os.userInfo();
-// console.log(user);
+var adding = notes.add(11,-9);
+console.log(adding);
 
-// appending text to the .txt file
-// if error occured - throw an error
-// if everything is ok - show a message to the console
-// user.username gets your OS' username (iftemplar)
-fs.appendFile('greetings.txt', `\nHello ${user.username}`, (err) => { // `\nHello ${user.username}` is ES6 template strings way of writing '\nHello ' + user.username
-	if (err) throw err;
-	  console.log('The "data to append" was appended to file!');
-});
+
+// fs.appendFile('greetings.txt', `\nHello ${user.username}! You are ${notes.age}`, (err) => { // `\nHello ${user.username}` is ES6 template strings way of writing '\nHello ' + user.username
+// 	if (err) throw err;
+// 	  console.log('The "data to append" was appended to file!');
+// });
 
