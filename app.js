70fb774d92dd579a0ae6cb3f1d425936aa03c898
iftemplar <iftemplar@gmail.com>
2017-04-16 14:19:09 +0300

    Using Yargs to add-list-read-remove a note

--- app.js
+++ app.js
@@ -7,20 +7,19 @@ const yargs = require('yargs');
 const notes = require('./notes.js');
 
 const argv = yargs.argv;
-var command = process.argv[2];
+var command = argv._[0];
 console.log('Command: ', command);
-console.log('Process: ', process.argv);
 console.log('Yargs', argv);
 
 
 if(command === 'add'){
-	console.log('Adding a note');
+	notes.addNote(argv.title, argv.body);
 } else if(command === 'list') {
-	console.log('Listing notes');
+	notes.listNotes();
 } else if(command === 'read') {
-	console.log('Reading a note');
+	notes.readNote(argv.title);
 } else if(command === 'remove') {
-    console.log('Removing a note');
+    notes.removeNote(argv.title);
 } else {
 	console.log('Command not recognized');
 }
